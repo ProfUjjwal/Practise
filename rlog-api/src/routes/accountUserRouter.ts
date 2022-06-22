33a35f72227cf0ebfbfe3a613507e1109b54c43e
ProfUjjwal <104754563+ProfUjjwal@users.noreply.github.com>
2022-06-23 00:42:56 +0530

    Delete rlog-api directory

--- rlog-api/src/routes/accountUserRouter.ts
+++
@@ -1,9 +0,0 @@
-import IZode from "@zopsmart/zode";
-import { sendInvitationHandler, acceptInvitationHandler, declineInvitationHandler, getUsersOfAccountHandler } from "../handlers/accountUserHandler";
-
-export default (app: IZode) => {
-  app.post("/account/:accountId/invite", sendInvitationHandler);
-  app.post("/account/:accountId/accept", acceptInvitationHandler);
-  app.put("/account/:accountId/decline", declineInvitationHandler);
-  app.get('/account/:accountId/users', getUsersOfAccountHandler);
-}
--- rlog-api/src/routes/logsRouter.ts
+++
@@ -1,6 +0,0 @@
-import IZode, { Context } from "@zopsmart/zode";
-import { createLogHandler, getLogsHandler } from "../handlers/logsHandler";
-export default (app: IZode) => {
-    app.post("/logs", createLogHandler);
-    app.get("/logs/:accountId", getLogsHandler);
-}
